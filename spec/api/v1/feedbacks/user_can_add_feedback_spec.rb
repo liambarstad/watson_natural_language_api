87606f84d823abebff46594e4fe19aa26a3da642
Liam Barstad <liambarstad@gmail.com>
2019-01-20 11:12:47 -0800

    add test

---
+++ spec/api/v1/feedbacks/user_can_add_feedback_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'User can add feedback', type: :request do
+  
+  it 'with valid credentials' do
+    post 'api/v1/feedbacks', params: { message: 'This is a test' }
+
+    expect(response.status).to eq(200)
+    expect(response.body[:message]).to eq('This is a test')
+    expect(Feedback.all.count).to eq(1)
+    expect(Feedback.first.message).to eq('This is a test')
+  end
+
+end
