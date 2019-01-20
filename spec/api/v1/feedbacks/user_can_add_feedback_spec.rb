092e9ebb31649da41521a5919a97a4f07603cf52
Liam Barstad <liambarstad@gmail.com>
2019-01-20 11:24:34 -0800

    add passing feedback test

--- spec/api/v1/feedbacks/user_can_add_feedback_spec.rb
+++ spec/api/v1/feedbacks/user_can_add_feedback_spec.rb
@@ -5,8 +5,10 @@ RSpec.describe 'User can add feedback', type: :request do
   it 'with valid credentials' do
     post '/api/v1/feedbacks', params: { message: 'This is a test' }
 
+    body = JSON.parse(response.body)
+
     expect(response.status).to eq(200)
-    expect(response.body['message']).to eq('This is a test')
+    expect(body['message']).to eq('This is a test')
     expect(Feedback.all.count).to eq(1)
     expect(Feedback.first.message).to eq('This is a test')
   end
