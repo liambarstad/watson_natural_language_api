9c26ea473584fd4ab8853f00f1c481c6e3b72132
Liam Barstad <liambarstad@gmail.com>
2019-01-20 11:47:09 -0800

    add test

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :feedbacks, only: [:create]
+      post 'get_token', to: 'users#create' 
     end
   end
 end
---
+++ spec/api/v1/users/user_can_create_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe 'User can generate access token', type: :request do
+  
+  it 'with non-existing username' do
+    post '/api/v1/get_token', params: { username: 'Liam' }
+
+    body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(body['token']).to_not be(nil)
+  end
+
+  it 'with existing username' do
+    User.create!(username: 'Liam')
+    post '/api/v1/get_token', params: { username: 'Liam' }
+    
+    body = JSON.parse(response.body)
+
+    expect(response.status).to eq(200)
+    expect(body['token']).to_not be(nil)
+  end
+end
