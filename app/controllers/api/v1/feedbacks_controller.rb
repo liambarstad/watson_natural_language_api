837f3c88e2c73eb995657330ec51dbf7b7c50fe8
Liam Barstad <liambarstad@gmail.com>
2019-01-20 11:21:34 -0800

    add pry

--- app/controllers/api/v1/feedbacks_controller.rb
+++ app/controllers/api/v1/feedbacks_controller.rb
@@ -3,4 +3,11 @@ class Api::V1::FeedbacksController < ApplicationController
     @feedback = Feedback.create!(feedback_params)
     render json: @feedback.to_json
   end
+
+  private
+
+    def feedback_params
+      params.permit(:message)
+    end
+
 end
