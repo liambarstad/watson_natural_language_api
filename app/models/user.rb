ce9a6ca18eec7d5dade4bbccb5523811f6a787ea
Liam Barstad <liambarstad@gmail.com>
2019-01-21 12:01:44 -0800

    add dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :api_key
   has_one :access_token, dependent: :destroy
-  has_many :feedbacks
+  has_many :feedbacks, dependent: :destroy
 
   def generate_access_token
     self.access_token = JWTService.encode_token(id, api_key)
