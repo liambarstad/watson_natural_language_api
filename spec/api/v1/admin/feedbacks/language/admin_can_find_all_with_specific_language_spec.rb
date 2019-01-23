efbe2fef1af8334ff11cd70d4c0e12935c515829
Liam Barstad <liambarstad@gmail.com>
2019-01-22 19:37:24 -0700

    add spec

--- spec/api/v1/admin/feedbacks/language/admin_can_find_all_with_specific_language_spec.rb
+++ spec/api/v1/admin/feedbacks/language/admin_can_find_all_with_specific_language_spec.rb
@@ -9,16 +9,51 @@ RSpec.describe 'Admin can find all feedbacks with specific language' do
     create(:french_feedback)
   end
   
-  it 'with listed language and correct token' do
+  describe 'with correct token' do
+
+    before :each do
+      let!(:token) { get_token(admin: true) }
+    end
+
+    it 'with english language' do
+      get '/api/v1/feedbacks', params: { lang: 'en', token: token } 
+
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      expect(body.length).to eq(1)
+    end
+
+    it 'with french language' do
+      get '/api/v1/feedbacks', params: { lang: 'fr', token: token } 
+
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq(200)
+      expect(body.length).to eq(1)
+    end
+
+    it 'with unlisted language' do
+      get '/api/v1/feedbacks', params: { lang: 'nada', token: token } 
+
+      body = JSON.parse(response.body)
+
+      expect(response.status).to eq(404)
+      expect(body['error']).to eq('Language not listed')
+      expect(body.keys.length).to eq(1)
+    end
 
   end
 
-  it 'with listed language and base user token' do
+  it 'with base user token' do
+    token = get_token
+    get '/api/v1/feedbacks', params: { lang: 'en', token: token } 
+    
+    body = JSON.parse(response.body)
 
-  end
-
-  it 'with unlisted language and correct token' do
-
+    expect(response.status).to eq(401)
+    expect(body['error']).to eq('Unauthorized')
+    expect(body.keys.length).to eq(1)
   end
 
 end
